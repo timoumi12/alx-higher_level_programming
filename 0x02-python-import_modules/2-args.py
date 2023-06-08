bc4adc7326036907946fab04a01e048cca5b70d6
timoumi12 <ihebtimoumi1@gmail.com>
2023-06-09 00:00:30 +0100

    import&modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+arg = argv
+if __name__ == "__main__":
+    if (len(argv) == 2):
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        if (len(argv) == 1):
+            print("0 arguments.")
+        else:
+            print("{} arguments:".format(len(argv) - 1))
+            for i in range(1, len(argv)):
+                print("{}: {}".format(i, argv[i]))
