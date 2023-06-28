64bacfa873054281985e0fce23033f017aa0d2bd
timoumi12 <ihebtimoumi1@gmail.com>
2023-06-28 18:30:15 +0100

    classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Defines Square class"""
+
+
+class Square:
+    """creates a square"""
+    def __init__(self, size=0):
+        """ Initializes a new square
+
+        Args:
+        size (int): the size of new square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """returns the size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """sets the size"""
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ Returns the square area"""
+        return (self.__size**2)
