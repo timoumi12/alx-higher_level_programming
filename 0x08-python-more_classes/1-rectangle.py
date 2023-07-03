fdde6c4c6d526d36183b793a301ae6e36e652fb0
timoumi12 <ihebtimoumi1@gmail.com>
2023-07-03 23:22:12 +0100

    classes 2.0

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""Defines Rectangle Class"""
+
+
+class Rectangle:
+    """Creates a Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Instantiation of a new Reactangle
+        
+        Args:
+        width (int): the width of new Rectangle
+        height (int): the height of new Rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """retrieves the width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """sets the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieves the height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """sets the height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
