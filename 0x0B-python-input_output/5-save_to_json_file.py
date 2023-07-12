34dc339cd9d5679d431d711d5301a5aaf15be5b4
timoumi12 <ihebtimoumi1@gmail.com>
2023-07-12 01:58:55 +0100

    python input output

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+"""4-from_json_string.py"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    deserializes an object
+    """
+    with open(filename, 'w') as f:
+        obj = json.dumps(my_obj, filename)
