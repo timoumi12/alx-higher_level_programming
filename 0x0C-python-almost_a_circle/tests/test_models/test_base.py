4aee1af6eba91cf4ad11297c30ac5cd8830fd910
timoumi12 <ihebtimoumi1@gmail.com>
2023-07-17 18:45:16 +0100

    Base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,1 +1,31 @@
+#!/usr/bin/python3
+"""tests/test_models/test_base.py"""
+import unittest
 
+
+class TestBase_instances(unittest.TestCase):
+    """testing the base class"""
+
+    def test_no_arg(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_two_bases(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_unique_id(self):
+        self.assertEqual(12, Base(12).id)
+
+    def test_nb_instances_after_unique_id(self):
+        b1 = Base()
+        b2 = Base(12)
+        b3 = Base()
+        self.assertEqual(b1.id, b3.id - 1)
+
+    def test_id_public(self):
+        b = Base(12)
+        b.id = 15
+        self.assertEqual(15, b.id)
