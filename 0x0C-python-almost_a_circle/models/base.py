d475a159b886238bf467253d273df67fa29ce216
timoumi12 <ihebtimoumi1@gmail.com>
2023-07-17 17:36:18 +0100

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""models/base.py"""
+
+
+class Base:
+    """Base Class"""
+
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """class constructor"""
+
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
