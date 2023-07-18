980175852f50f19ece0b81af6deb2c53c74dd45e
timoumi12 <ihebtimoumi1@gmail.com>
2023-07-18 02:32:18 +0100

    Base class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """models/square.py"""
-from models.base import Base
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """Square Class"""
 
-    __nb_objects = 0
 
     def __init__(self, size, x=0, y=0, id=None):
         """class constructor"""
@@ -16,17 +15,17 @@ class Square(Rectangle):
     @property
     def size(self):
         """get the size"""
-        return self.__height
+        return self.height
 
-    @width.setter
-    def width(self, value):
+    @size.setter
+    def size(self, value):
         """set the size"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
-        self.__height = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """returns the print() and str() representation of the Rectangle."""
