df334f7f93f951462a9935761bfa7b20b57d088b
timoumi12 <ihebtimoumi1@gmail.com>
2023-07-18 12:13:22 +0100

    python input output

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """models/base.py"""
+import json
 
 
 class Base:
@@ -15,3 +16,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        _str = "["
+        if len(list_dictionaries) == 1:
+            _str += json.dumps(list_dictionaries[0]) + "]"
+        else:
+            for i in range(list_dictionaries) - 1:
+                _str += json.dumps(list_dictionaries[i]) + ", "
+            _str += json.dumps(list_dictionaries) + "]"
+        return _str
