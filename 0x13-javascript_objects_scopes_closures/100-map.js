e7607f908a68106adb48963938e25f22be4b2775
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-13 22:30:58 +0100

    fix error

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const _list = require('100-data.js').list;
-const newlist = _list.map(
-  function (num, index) {
-    return num * index;
+const list = require('100-data.js').list;
+const newlist = list.map(
+  function (value, i) {
+    return i * value;
   }
 );
-console.log(_list);
+console.log(list);
 console.log(newlist);
