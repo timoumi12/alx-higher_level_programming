02afa886319e4cd84d7cb77920f954442e4cfc5c
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-15 10:44:51 +0100

    initial commit : README file and 1st task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''lists all states from the database hbtn_0e_0_usa'''
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states;")
+    data = cur.fetchall()
+    for d in data:
+      print(d)
+    cur.close()
+    conn.close()
