81303e9be1c721b99a52b4f3e7b2e1724c0d96cb
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-17 13:05:20 +0100

    last task

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
+
 class City(Base):
     """City Class"""
     __tablename__ = "cities"
