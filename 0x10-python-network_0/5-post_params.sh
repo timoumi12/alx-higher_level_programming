b2bfc95dafad618f08b7aaf45e182015b6f05924
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-28 21:48:16 +0100

    post req

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -H "email: test@gmail.com" -H "subject: I will always be here for PLD" -sX POST $1 -L
+curl -d "email: test@gmail.com&subject: I will always be here for PLD" -sX POST $1 -L
