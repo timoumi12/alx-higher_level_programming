7b5728b2499b2fb49df1bf4dceeb128da49e512f
Iheb Timoumi <55882164+timoumi12@users.noreply.github.com>
2023-09-28 21:28:14 +0100

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sI ALLOW $1 -L
+curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
