46118855923485a4c2360b889afeba8233a2cd75
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-29 17:20:02 +0100

    network 2.0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''takes in a URL, sends a request to the URL and displays the response'''
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    url = sys.argv[1]
+    try:
+        req = urllib.request.Request(url)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+    with urllib.request.urlopen(req) as response:
+        resp = response.read()
+        data = resp.decode('utf-8')
+        print(data)
