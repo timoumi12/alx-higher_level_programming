60caa3c70ea18a125eb7be1c012e607324d47a83
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-29 18:15:23 +0100

    Hot Fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,5 @@ if __name__ == "__main__":
     import sys
 
     url = sys.argv[1]
-    email = {'email': sys.argv[2]}
-    req = requests.get(url, params=email)
+    req = requests.post(url, params={'email': sys.argv[2]})
     print(req.text)
