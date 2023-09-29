89d4bac5851d91205c237f3d4dffa708df8240c6
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-29 16:26:13 +0100

    hot fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 '''a script takes a URL,
  sends a request and displays the value of X-Request-Id found in the header'''
-import urllib.request
-import sys
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
     with urllib.request.urlopen(sys.argv[1]) as response:
         value = response.headers.get("X-Request-Id")
         print(value)
