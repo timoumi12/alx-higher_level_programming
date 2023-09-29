0879cfe0262cad32abab9a4eec3dc2cfd8e7138e
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-29 16:15:18 +0100

    network 2.0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,8 @@
 
 if __name__ = "__main__":
     import urllib.request
+
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         data = html.decode('utf-8')
