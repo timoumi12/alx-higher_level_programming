8652a81c5810a563d9231e8e9c9dc7932fb67a57
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-29 16:14:14 +0100

    network 2.0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 '''a script that fetches https://alx-intranet.hbtn.io/status'''
-import urllib.request
+
 
 if __name__ = "__main__":
+    import urllib.request
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         data = html.decode('utf-8')
