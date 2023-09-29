31c48eb61832750c6a6ba22556997e28da7374ac
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-29 18:47:16 +0100

    network 2.0

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''8-json_api.py'''
+
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = "http://0.0.0.0:5000/search_user"
+    char = {'q': ""}
+    if sys.argv[1]:
+        char = {'q': sys.argv[1]}
+    req = requests.get(url, data=char)
+    try:
+        req = req.json
+        if req == {}
+            print("No result")
+        else:
+            print("[{}] {}".format(req.get("id"), req.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
