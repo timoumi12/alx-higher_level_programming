5c771da5e247ce2688b96d660c001acce5016fce
timoumi12 <ihebtimoumi1@gmail.com>
2023-09-29 17:34:49 +0100

    Hot Fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     url = sys.argv[1]
     try:
         req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            resp = response.read()
+            data = resp.decode('utf-8')
+            print(data)
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
         return
-    with urllib.request.urlopen(req) as response:
-        resp = response.read()
-        data = resp.decode('utf-8')
-        print(data)
