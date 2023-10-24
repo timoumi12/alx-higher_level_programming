a358534d473b2aee6c3443e789aa53b9d99632e9
timoumi12 <ihebtimoumi1@gmail.com>
2023-10-24 18:55:24 +0100

    hot fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 
 // read file
 fs.readFile(process.argv[2], 'utf-8', function (err, data) {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
