ae90b5208001942a7b8e5b1c8066a4ee61f553d3
timoumi12 <ihebtimoumi1@gmail.com>
2023-10-24 19:45:42 +0100

    hot fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,5 +4,9 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
 request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
   console.log(JSON.parse(body).title);
 });
