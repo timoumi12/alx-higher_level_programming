d4d8eecd3edf14e0bced45ec844447954315dad3
Iheb Timoumi <55882164+timoumi12@users.noreply.github.com>
2023-11-01 21:52:15 +0100

    Create 8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.get(
+  'https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    $.each(data.results, function (i, film) {
+      $('UL#list_movies').append('<li>' + film.title + '</li>');
+    });
+  });
